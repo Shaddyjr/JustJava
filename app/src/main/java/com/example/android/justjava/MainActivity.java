d428f138c2abec2f0b1ca0c28d0cffbddd64abe6
mahdiASC <mahdi@allstarcode.org>
2018-02-11 22:16:09 -0500

    Added email intent and spanish languange

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,12 +9,15 @@
 package com.example.android.justjava;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -27,49 +30,89 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int x = 2;
+    int x = 1;
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-//        displayPrice(x * 5);
-        String priceMessage = "Total = $" + (x * 5) + "\nThank you!";
-        displayMessage(priceMessage);
+        int price = calculatePrice();
+        createOrderSummary(price);
     }
     /**
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
-        display(++x);
+        if(x<=100){
+            displayQuantity(++x);
+        }else{
+            Toast.makeText(this,"You cannot have more than 100 coffees",Toast.LENGTH_SHORT).show();
+        }
+
     }
     /**
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
-        display(--x);
+        if(x>2){
+            displayQuantity(--x);
+        }else{
+            Toast.makeText(this,"You cannot have less than 1 coffee",Toast.LENGTH_SHORT).show();
+        }
     }
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
+
     /**
-     * This method displays the given price on the screen.
+     * This method returns a string of the order summary
+     * @param price of all coffees ordered
+     * @return text summary
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private void createOrderSummary(int price){
+        CheckBox whip = (CheckBox) findViewById(R.id.whip);
+        CheckBox choc = (CheckBox) findViewById(R.id.choc);
+        EditText _name = (EditText) findViewById(R.id.userName);
+        String name = _name.getText().toString();
+
+//        using strings.xml as resource
+        String text = getString(R.string.sum_name, name);
+        text += "\n" + getString(R.string.sum_whip, whip.isChecked());
+        text += "\n" + getString(R.string.sum_choc, choc.isChecked());
+        text += "\n" + getString(R.string.sum_quant, "" + x);
+        text += "\n" + getString(R.string.sum_total, "" + price);
+        text += "\n" + getString(R.string.sum_thanks);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:udacity@udacity.com")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
-     * This method displays the given text on the screen.
+     * Calculates the price of the order.
+     *
      */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+    private int calculatePrice() {
+        CheckBox whip = (CheckBox) findViewById(R.id.whip);
+        CheckBox choc = (CheckBox) findViewById(R.id.choc);
+        int output = 5;
+        if(whip.isChecked()){
+            output += 1;
+        }
+
+        if(choc.isChecked()){
+            output += 2;
+        }
+        return x * output;
     }
+
 }
 
